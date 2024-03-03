cebe8a2d9f78fd2e88fa3d0f0527fd6fcb4041ab
Sami Aji <sami@ajility.dev>
2024-03-02 20:37:57 -0500

    feat: add example

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="width: 40%">
+    <awesome-image-gallery
+      :images="[
+        {
+          url: 'https://cdn11.bigcommerce.com/s-j38el1cgyu/images/stencil/1280x1280/products/7462/24151/B-3793_0__72902.1689395806.png?c=1',
+        },
+        {
+          url: 'https://cdn11.bigcommerce.com/s-j38el1cgyu/images/stencil/1280x1280/products/7462/24152/B-3793_1__50815.1689395808.png?c=1',
+        },
+        {
+          url: 'https://cdn11.bigcommerce.com/s-j38el1cgyu/images/stencil/1280x1280/products/7462/24153/B-3793_2__17221.1689395810.png?c=1',
+        }
+      ]"
+      :scale="0.9"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AwesomeImageGallery from './AwesomeImageGallery.vue'
+</script>
